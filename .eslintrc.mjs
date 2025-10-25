f19f8588dcc0c95d117af106c4096401361c083b
Fernanda Castillo <78625853+feercastillob@users.noreply.github.com>
2025-10-25 17:44:33 -0600

    chore: update ESLint and Prettier configurations for improved code quality and consistency

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -1,29 +1,50 @@
-/* eslint-env node */
-import '@rushstack/eslint-patch/lib/patch-eslint';
-
 export default {
   root: true,
   env: {
     browser: true,
-    es2020: true,
+    es2021: true,
     node: true,
-    'vue/setup-compiler-macros': true,
   },
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
-    '@vue/eslint-config-typescript',
-    'plugin:tailwindcss/recommended',
-    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
     'vue/multi-word-component-names': 'off',
-    'vue/script-setup-uses-vars': 'error',
-    'tailwindcss/classnames-order': 'warn',
-    'tailwindcss/no-custom-classname': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 3,
+        multiline: { max: 1, allowFirstLine: false },
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'lf',
+      },
+    ],
   },
 };
